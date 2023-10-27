9c56112bf8d9bf6dd7306cada1b27240e2f86e01
danizavtz <daniellucena@yahoo.com.br>
2023-10-27 14:39:42 -0300

    Fix urls.py

--- djangostk/urls.py
+++ djangostk/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import (handler404, handler500)
 from tplt.views import custom_404_view, default
 handler404 = custom_404_view
 urlpatterns = [
